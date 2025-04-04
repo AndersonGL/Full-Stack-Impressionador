5ac5b4bf17c6e3c8d8f9789c8a4f526aaed16d9f
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-04 15:25:34 -0300

    update js strings

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,6 +1,39 @@
-console.log("Hello World");
+// STRING
+
+//"Hello World!" - string
+
+//'Criar uma mensagem' string
+
+//`Criar mais um texto` - string template
 
 
+let mensagem = "Olá, Mundo!"
+
+let nome = 'Lucas';
+
+let mensagem2 = `Olá, {nome}`;
+
+console.log(mensagem)
+
+console.log(nome)
+
+console.log(`Olá, ${nome} Bem Vindo!`)
 
 
+//  INDICE COMEÇANDO POR 0 
+
+console.log(nome[3]) // a
+
+
+// NUMBER 
+
+let numero = 30;
+
+let numero1 = -56;
+
+let decimal = 5.5;
+
+console.log (numero, numero1, decimal)
+
+// infinity e NaN
 
