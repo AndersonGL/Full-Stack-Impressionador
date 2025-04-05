51ef656cbbd0be14ddc0a097f7d60fbce777700b
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-04 21:40:46 -0300

    update js

---
+++ JAVASCRIPT/undefeineAndNull.js
@@ -0,0 +1,19 @@
+let mensagem = "Olá ipressionador"
+let preço = 50.0;
+let logado = true;
+
+// Undefined
+
+let produto = undefined; // Undefined
+let carrinho = undefined; 
+console.log(typeof produto);
+console.log(typeof carrinho); 
+
+
+
+// Null
+
+let lista = null;  // Null
+console.log(lista); // object
+console.log(typeof lista); // object
+
