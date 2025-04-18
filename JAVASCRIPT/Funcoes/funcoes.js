d76421cb8771c516b9f5698009779d57e31dd929
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-18 11:41:16 -0300

    Update funcoes.js

--- JAVASCRIPT/Funcoes/funcoes.js
+++ JAVASCRIPT/Funcoes/funcoes.js
@@ -38,9 +38,109 @@ function main() {
 
     enviarMensagem();
     cadastroUsuario("Anderson ","Gouveia");
-    let saldo = banco(1000, 60);
+    let saldo = banco(1000, 780);
     console.log(`Seu saldo é de ${saldo} reais`);
 
 }
 
 main(); // Para continuar você prescisa informar o seu nome parra cadastro! Anderson Gouveia voce foi casadtrado com sucesso! Seu saldo é de 940 reais.
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Parâmetros e Argumentos
+// function nome funcao(PARAMÊTRO) {Instrução}
+// nomeFunction (Argumento) - Chamada da função (Executa o bloco de instruções)
+
+
+
+function soma(numero1, numero2){
+
+
+
+console.log(numero1 + numero 2);
+
+
+
+}
+
+
+
+soma(10, 5);
+
+
+
+
+
+function calcularPrecoTotal(precoUnitario , quantidade) {
+
+
+
+   let total = precoUnitario + quantidade;
+
+
+
+console.log("O total da sua compra é: " + total);
+
+
+
+}
+
+
+
+let camiseta = 30;
+
+let quantidadeItem = 3;
+
+
+
+calcularPrecoTotal (camiseta , quantidadeItem);
+
+
+calcularTotal();
+
+
+
+----------------------------------------------------------------------------------------------------------------
+// RETORNO DE UMA  FUNÇÃO
+
+let pedido = {
+id : 1234,
+
+nome : "João",
+
+email : "joao@exemplo.com",
+
+batataFrita : 6,
+
+suco : 4,
+
+
+
+};
+
+
+function enviarNotificacao(nome, idPedido, email) {
+
+
+
+console.log(`Enviando email para ${email} confirmado o pedido de número  ${idPedido}`)
+
+console.log(`Mensagem: ${nome} pedido confirmado`);
+
+}
+
+enviarNotificacao(pedido.nome, pedido.id, pedido.email);
+
+
+
+
+
+
+
+
+
+
+
+
+
