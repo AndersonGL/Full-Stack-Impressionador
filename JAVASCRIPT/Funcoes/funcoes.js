9d86d90056389557477dec9039aa65448c5ea40b
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-18 14:33:42 -0300

    Update funcoes.js

--- JAVASCRIPT/Funcoes/funcoes.js
+++ JAVASCRIPT/Funcoes/funcoes.js
@@ -104,33 +104,49 @@ calcularTotal();
 ----------------------------------------------------------------------------------------------------------------
 // RETORNO DE UMA  FUNÇÃO
 
+// Retorno de uma função
+
 let pedido = {
-id : 1234,
-
-nome : "João",
-
-email : "joao@exemplo.com",
-
-batataFrita : 6,
-
-suco : 4,
-
-
-
+  id: 1234,
+  nome: "João",
+  email: "joao@example.com",
+  lanche: 12,
+  batataFrita: 6,
+  suco: 4,
 };
 
-
 function enviarNotificacao(nome, idPedido, email) {
-
-
-
-console.log(`Enviando email para ${email} confirmado o pedido de número  ${idPedido}`)
-
-console.log(`Mensagem: ${nome} pedido confirmado`);
-
+  // Simular o envio de um email
+  console.log(
+    `Enviando email para ${email} confirmando o pedido do número ${idPedido}`
+  );
+  console.log(`Mensagem: ${nome} pedido confirmado`);
+  //Não possui retorno explícito
 }
 
-enviarNotificacao(pedido.nome, pedido.id, pedido.email);
+// enviarNotificacao(pedido.nome, pedido.id, pedido.email);
+
+// Função principal que irá calcular e retornar o valor do pedido
+
+function processarPedido(id, item1, item2, item3) {
+  let totalPedido = item1 + item2 + item3;
+  console.log("Pedido: " + id + " Processando");
+  return totalPedido; // encerrou a nossa função
+  // console.log("O total do pedido é: " + totalPedido); - Esta linha de código Não será lida
+}
+
+let retornoDaFuncao = processarPedido(
+  pedido.id,
+  pedido.lanche,
+  pedido.batataFrita,
+  pedido.suco
+);
+
+console.log(retornoDaFuncao ); 
+
+//Pedido: 1234 Processando
+
+//22
 
 
 
