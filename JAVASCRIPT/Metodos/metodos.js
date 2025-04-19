5736674f53451575f46e20f9221f02381b1eeb39
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-19 18:50:21 -0300

    Update metodos.js

--- JAVASCRIPT/Metodos/metodos.js
+++ JAVASCRIPT/Metodos/metodos.js
@@ -13,4 +13,16 @@ return `Olá, ${pessoa.nome}`;
 console.log(pessoa.saudacao());
 
 
-// String e Sequência de caractere
+// String e Sequência de caracteres
+
+
+let string = "Ola, mundo!";
+
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+
+// splice, (start e end)
+
+console.log(string.slice(0,5));  // conta do zero ao 
+console.log(string.slice(-6));   // conta de traz 
