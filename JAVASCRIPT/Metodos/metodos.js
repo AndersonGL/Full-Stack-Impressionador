1eb9665bc77fcef5f7a50bb4aa25480a1ad7bb96
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-19 17:07:18 -0300

    Update metodos.js

--- JAVASCRIPT/Metodos/metodos.js
+++ JAVASCRIPT/Metodos/metodos.js
@@ -1,1 +1,16 @@
 // Diferenças entre métodos e funções
+
+
+const pessoa = {
+
+  nome: "Anderson",
+  saudacao: function(){
+
+return `Olá, ${pessoa.nome}`;
+  }
+}
+
+console.log(pessoa.saudacao());
+
+
+// String e Sequência de caractere
