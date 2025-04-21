91c6a3fbf8dc31a25bf279b73dbacb26d2f85fe9
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-21 16:11:52 -0300

    Update exercicios1.js

--- JAVASCRIPT/Metodos/exercicios1.js
+++ JAVASCRIPT/Metodos/exercicios1.js
@@ -44,3 +44,14 @@ console.log(resultado)
 //Exercício 4: Dada a string 'cadeira, mesa, sofá, armário', divida essa string em um array de
 //produtos e remova o item 'sofá' da lista.
 //Exemplo de Saída: “["cadeira", "mesa", "armário"]”
+
+
+
+
+
+
+
+let itens = "cadeira", "mesa", "armário", "sofá"; 
+let sala = itens.split(",", 3);
+console.log(sala)
+
