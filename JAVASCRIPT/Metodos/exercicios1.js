f69292a3190dd2f499483c9d61dcfa68bd12ed73
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-21 15:20:04 -0300

    Create exercicios1.js

---
+++ JAVASCRIPT/Metodos/exercicios1.js
@@ -0,0 +1,18 @@
+//Exercício 1: Dada a string 'javascript é divertido', converta a palavra 'javascript' para letras
+//maiúsculas.
+
+
+//Exemplo de Saída: // “JAVASCRIPT é divertido”
+//Exercício 2: Dada a string "Aprendendo Javascript", extraia a palavra "Aprendendo" e,
+//em seguida, converta essa palavra para letras minúsculas.
+//Exemplo de Saída: // “ aprendendo”
+
+
+//Exercício 3: Dada a string "Aprendendo Python", extraia a palavra "Python" e substitua por
+//“Javascript”.
+//Exemplo de Saída: // “Aprendendo Javascript”
+
+
+//Exercício 4: Dada a string 'cadeira, mesa, sofá, armário', divida essa string em um array de
+//produtos e remova o item 'sofá' da lista.
+//Exemplo de Saída: “["cadeira", "mesa", "armário"]”
