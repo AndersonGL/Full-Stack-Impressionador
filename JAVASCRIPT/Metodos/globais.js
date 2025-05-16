02df49147f3a6abbaff376946f0c8f83a18797b4
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-15 21:20:26 -0300

    update globais

---
+++ JAVASCRIPT/Metodos/globais.js
@@ -0,0 +1,11 @@
+// Math = operações matemáticas e Date = datas
+// PI = 2.14159...
+
+const PI = Math.PI;
+console.log(PI);
+
+const raizQuadrada = Math.sqrt(16); // 4
+console.log(raizQuadrada);
+
+const potencia = Math.pow(2, 4); // 16
+console.log(potencia);
