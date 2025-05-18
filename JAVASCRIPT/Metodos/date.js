946c9dc6180604ea09db1a8dbc7869ec5f81b116
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-18 16:14:22 -0300

    update

---
+++ JAVASCRIPT/Metodos/date.js
@@ -0,0 +1,38 @@
+// Classe vc. Instância
+
+
+class Carro {
+
+marca= "Toyota";
+modelo = "Corolla";
+ano = 2024;
+
+
+ligar() {
+
+    console.log("Carro ligado");
+
+}
+
+desligar() {
+
+    console.log("Carro desligado");
+}
+
+exibirInformacoes() {
+   
+    console.log(`O carro é de modelo: ${Carro.modelo}
+     da marca ${Carro.marca}, do ano de ${Carro.ano}`
+    
+    );
+  }
+}  
+
+const carro = {
+
+    modelo: "Corolla",
+    marca: "Toyota",
+    ano: 2024,
+};
+
+console.log(carro);
---
+++ JAVASCRIPT/Metodos/date2.js
@@ -0,0 +1,25 @@
+
+const dataAtual = new Date();
+console.log(dataAtual.getFullYear()); // ano atual
+console.log(dataAtual.getMonth()); // mes atual - 0 a 11 (0 será  Janeiro e 11 - Será Dezembro)
+console.log(dataAtual.getDate());   // dia atual  
+console.log(dataAtual.getHours());  // hora atual
+console.log(dataAtual.getMinutes()); // minutos atuais
+console.log(dataAtual.getSeconds()); // segundo atuais
+
+console.log(dataAtual);
+
+// timestamp - representação numérica - data referência 1 de janeiro
+//de 1970
+
+let timestamp = dataAtual.getTime();
+//console.log(dataAtual);
+//console.log(timestamp);
+// timestamp - representação numérica - data referência 1 de janeiro
+//d
+//console.log(New Date(172988332187));
+//consollog(New Date(172988378522));
+
+
+let agora = new Date("2024-10-07");
+console.log(agora);
