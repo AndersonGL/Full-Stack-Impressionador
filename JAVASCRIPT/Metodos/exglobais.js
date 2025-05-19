62e48652e77a4cc2fa8b99dc3ca14bc632546a94
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-19 20:17:02 -0300

    update globais

---
+++ JAVASCRIPT/Metodos/exglobais.js
@@ -0,0 +1,27 @@
+// Exercício 1: Você precisa criar uma função que receba um número decimal e o arredonde de
+// três formas diferentes: para baixo, para cima e para o valor inteiro mais próximo. Mostre o
+// resultado de cada arredondamento no console.
+// Exemplo Entrada: // 7.89
+// Exemplo Saída: // Arredondado para baixo: 7
+// Arredondado para cima: 8
+// Arredondado para o valor mais próximo: 8
+
+
+
+// Exercício 2: Crie uma função que receba uma lista de números e retorne o valor máximo e o
+// valor mínimo dessa lista.
+// Exemplo Entrada: // 10, 5, 20, 40, 1, 7
+// Exemplo Saída: // Valor mínimo: 1
+// Valor máximo: 40
+
+
+
+// Exercício 3: Você deve criar uma função que gere três tipos de números aleatórios: um entre 0
+// e 1, outro entre 0 e 100, e um número inteiro aleatório entre dois valores específicos. Exiba os
+// resultados no console.
+// Exemplo Entrada: // 50, 100
+// Exemplo Saída: // Aleatório entre 0 e 1: 0.8629821531529918
+// Aleatório entre 0 e 100: 42.48780566605361
+// Aleatório entre 50 e 100: 63.70589822740784
+
+
