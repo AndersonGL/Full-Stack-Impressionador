1bedfaf747b354a7925af4d60dcc11cc2c732317
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-20 18:21:47 -0300

    update

---
+++ BACK END/Modulo Node.js/script.js
@@ -0,0 +1,15 @@
+console.log("Olá, boa noite!");
+
+function addFiveNumbers(number1, number2, number3, number4, number5) {
+  return number1 + number2 + number3 + number4 + number5;
+}
+
+global.console.log("Olá, Feliz ano novo!");
+
+let pais = "Brasil";
+
+function selectBestCountry() {
+  return "Brasil";
+}
+
+selectBestCountry(pais);
