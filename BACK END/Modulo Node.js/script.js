8eaa03145ab7c800c0af3bc956b2da9399d4902a
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-05-20 21:49:18 -0300

    update back

--- BACK END/Modulo Node.js/script.js
+++ BACK END/Modulo Node.js/script.js
@@ -1,15 +1,8 @@
-console.log("Olá, boa noite!");
+let numero = 10;
 
-function addFiveNumbers(number1, number2, number3, number4, number5) {
-  return number1 + number2 + number3 + number4 + number5;
+function teste(numero) {
+  console.log(numero);
+  return numero;
 }
 
-global.console.log("Olá, Feliz ano novo!");
-
-let pais = "Brasil";
-
-function selectBestCountry() {
-  return "Brasil";
-}
-
-selectBestCountry(pais);
+teste(numero);
