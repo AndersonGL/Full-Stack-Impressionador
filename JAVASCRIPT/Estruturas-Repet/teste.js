8822f52473f93d092f33978605b77e277bbb70f1
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-06-02 20:34:04 -0300

    update

---
+++ JAVASCRIPT/Estruturas-Repet/teste.js
@@ -0,0 +1,11 @@
+// While
+
+let numero = 1;
+
+while (numero <= 5) {
+  console.log("Número" + numero);
+  numero++;
+  b; // numero vai somar +1
+}
+
+console.log("contagem com while concluida!");
